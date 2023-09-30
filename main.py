ce7393411ee01330275f23b5451ce5bc028c3943
Hushianasan <hima_wariz@yahoo.co.jp>
2023-09-30 17:31:00 +0900

    自分が書いたコード

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+# -*- coding: utf-8 -*-
+
 import os
 import requests
 import json
 
-print(os.getcwd())
-
 # 気象庁データの取得
 url = 'https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json'
 response = requests.get(url)
-data = response.json()
+date = response.json()
+str(date)
 
-print("response : ", json.dumps(data, indent=2))
+print("response : ", date)
