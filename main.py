1ea3f027801507e007c3c9c7b87e63aaaeb1bf3c
Hushianasan <hima_wariz@yahoo.co.jp>
2023-09-30 17:31:47 +0900

    ChatGPTに綺麗にしてもらったコード

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-# -*- coding: utf-8 -*-
-
-import os
 import requests
-import json
 
 # 気象庁データの取得
 url = 'https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json'
 response = requests.get(url)
-date = response.json()
-str(date)
+data = response.json()
 
-print("response : ", date)
+# レスポンスを文字列に変換して表示
+print("response : ", data)
