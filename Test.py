21d2439161fa41af55e7075a53721ad82b0242f3
Hushianasan <hima_wariz@yahoo.co.jp>
2023-10-02 15:36:34 +0900

    @hushianasanが闇雲に追加した不潔なコード

---
+++ Test.py
@@ -0,0 +1,15 @@
+from scrape import scrape_weather_data
+
+# scrape_weather_data(2004,1,47887)
+
+#n = int(input("Input : "))
+#m = 0
+
+#str year, month, block_info = 0
+
+'''year = input("year : ")
+month = input("month : ")
+block_info = input("block_info : ")'''
+#scrape_weather_data(year, month, block_info)
+scrape_weather_data(2004, 1, 47887)
+
--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-# 松山, 西条市, 宇和島市の出力が出来る関数
+from scrape import scrape_weather_data
+
+# 松山市, 西条市, 宇和島市の出力が出来る関数
 # ３地点の指定をさせて出力する関数
 
-print("Start ! !\n")
-print("1. 年のみ指定\n")
-print("2. 年月のみ指定\n")
+print("Start ! !")
+print("1. 年のみ指定 ! !")
+print("2. 年月のみ指定 ! !")
 n = int(input("Input : "))
 
-if n == 1:
-	print("年のみ指定 ! !\n")
-	m = int(input("Input : ")) #松山, 西条市, 宇和島 地点の指定
-	# mを関数につなげて結果返す
-elif n == 2:
-	print("年月のみ指定 ! !\n")
-	m = int(input("Input : ")) # 松山, 西条市, 宇和島 地点の指定
-	# mを関数につなげて結果返す 
-else:
-	print("Error")
-# do whileで組む
+#str year, month, block_info 多分使わない
+j = 0
+
+while True:
+	if n == 1:
+		# n = 1
+		print("年のみ指定 ! !")
+		year = input("year : ")
+		month = input("month : ")
+		block_info = input("block_info : ")
+		scrape_weather_data(year, month, block_info)
+		
+
+	elif n == 2:
+		# n = 2
+		print("年月のみ指定 ! !")
+		year = input("year : ")
+		month = input("month : ")
+		block_info = input("block_info : ")
+		scrape_weather_data(year, month, block_info)
+
+	else:
+		print("Error")
+		break
+		
