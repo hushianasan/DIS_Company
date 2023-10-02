0f2f4bb562d3aaee0e44a94d0b9760d7ac14be7c
Hushianasan <hima_wariz@yahoo.co.jp>
2023-10-02 23:58:27 +0900

    @igz0が書いたコードを読みながら書いたコード

--- NWTest.py
+++ NWTest.py
@@ -1,9 +1,40 @@
 import requests
+from be4 import BeautifulSoup
 
-# 気象庁データの取得
-url = 'https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json'
-response = requests.get(url)
-data = response.json()
+# year
+# month
+# block_no
 
-# レスポンスを文字列に変換して表示
-print("response : ", data)
+def scrape_weather_data(year, month, block_no):
+
+    if block_no == "0958":
+    # 西条市
+        base_url = https://www.data.jma.go.jp/obd/stats/etrn/view/daily_a1.php
+    else:
+    # 松山市, 宇和島
+        base_url = https://www.data.jma.go.jp/obd/stats/etrn/view/daily_s1.php
+
+    block_no = {
+        '47887': '松山',
+        '47892': '宇和島',
+        '0958':  '西条'
+    }
+
+    params = {
+        'prec_no': '73',
+        'block_no': block_no,
+        'year': year,
+        'month': month,
+        'day': '',
+        'view': 'p1'
+    }
+
+    response = requests.get(base_url, params=params)
+
+        # URLにparamsを付与させる
+        base_url = 
+
+        html = requests.get(url)
+
+    # Webスクレイピングする
+    
---
+++ Test.py
@@ -0,0 +1,3 @@
+from NWTest import scrape_weather_data
+
+scrape_weather_data(2004, 1, 47887)
